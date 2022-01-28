4417dcae3f356ee139b2d2cc40c593e942d0b8d8
Zachary Runge Cormack <cormack.zachary@gmail.com>
2022-01-28 15:48:05 -0500

    added basic shooter math function

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,49 +1,34 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import com.arctos6135.robotpathfinder.core.RobotSpecs;
 
 import edu.wpi.first.wpilibj.XboxController;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
 
     // SPARK MAX Motors 
-    // TODO: Placeholder values as of now 
     public static final int RIGHT_CANSPARKMAX = 1;
     public static final int LEFT_CANSPARKMAX = 2;
     public static final int RIGHT_CANSPARKMAX_FOLLOWER = 3;
     public static final int LEFT_CANSPARKMAX_FOLLOWER = 4;
 
     // Shooter Motors 
-    // TODO: Write constants for shooter motors 
+    public static final int MAIN_SHOOTER_MOTOR = 5;
+    public static final int AUXILLIARY_SHOORTER_MOTOR = 6;
 
     // Intake Motors 
-    // TODO: Placeholder values as of now 
-    // TODO: check final robot intake subsystem design 
     public static final int LEFT_INTAKE_MOTOR = 0;
     public static final int RIGHT_INTAKE_MOTOR = 0; 
 
-    // SPARK MAX Encoders 
-    // TODO: change gearbox ratio 
-    public static final double WHEEL_DIAMETER = 4.0; // inches 
-    public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI; // inches 
+    // SPARK MAX Encoders (in inches)
+    public static final double WHEEL_DIAMETER = 4.0;
+    public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
     public static final double GEARBOX_RATIO = 0;
-    public static final double POSITION_CONVERSION_FACTOR = WHEEL_CIRCUMFERENCE * GEARBOX_RATIO; // inches 
-    public static final double VELOCITY_CONVERSION_FACTOR = WHEEL_CIRCUMFERENCE * GEARBOX_RATIO / 60; // inches / second
+    public static final double POSITION_CONVERSION_FACTOR = WHEEL_CIRCUMFERENCE * GEARBOX_RATIO;
+    public static final double VELOCITY_CONVERSION_FACTOR = WHEEL_CIRCUMFERENCE * GEARBOX_RATIO / 60;
     public static final int COUNTS_PER_REVOLUTION = 42;
     
-    // Warning and Shutoff Temperatures 
+    // Warning and Shutoff Temperatures (in centigrade, for inconsistency)
     public static double MOTOR_WARNING_TEMP = 70;
     public static double MOTOR_SHUTOFF_TEMP = 90;
     
